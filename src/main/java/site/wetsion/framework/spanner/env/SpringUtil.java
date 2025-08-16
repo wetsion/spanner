3f062a4be15d36b11cb206b6b92f77a4b3ed1af6
Wetsion <13110018525@163.com>
2025-08-16 21:47:53 +0800

    SpringUtil

---
+++ src/main/java/site/wetsion/framework/spanner/env/SpringUtil.java
@@ -0,0 +1,38 @@
+package site.wetsion.framework.spanner.env;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * spring工具类
+ * @author wetsion
+ */
+@Component
+public class SpringUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return applicationContext.getBean(name, clazz);
+    }
+
+    public static <T> T getBean(String name) {
+        //noinspection unchecked
+        return (T) applicationContext.getBean(name);
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
